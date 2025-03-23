acf0b4e2b986b5e7d2c45b1eaca962b2759c00eb
NotTacoz <tacozpvp@gmail.com>
2025-03-23 16:26:15 +0800

    page 44 - learnt variables

--- pun.c
+++ pun.c
@@ -1,7 +1,21 @@
+/*Name: pun.c*/
+/*Purpose: Prints a bad pun.*/
+/*Author: K. N. King*/
 #include <stdio.h>
 
 int main(void)
 {
-  printf("To C, or not to C: that is the question.\n");
+  int height = 8; // defining an integer variable
+  float profit; // a float type variable
+  int length, width, volume; // multiple variables defining
+
+  length = 2;
+  width = 10;
+
+  volume = height * length * width;
+  
+  // decleration + statement goes in here
+  printf("To C, or not to C: that is the question. Also the volume is %d\n", volume);
   return 0;
-}
+} // this is the second type of comment !
+
