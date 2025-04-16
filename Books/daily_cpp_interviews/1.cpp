3f51ff7c9d9c218a97506682f7cbac9b4489dbc1
NotTacoz <tacozpvp@gmail.com>
2025-04-16 09:18:20 +0800

    wow

---
+++ Books/daily_cpp_interviews/1.cpp
@@ -0,0 +1,13 @@
+// Question 1
+//
+// What are auto type deductions?
+//
+
+
+int* ip;
+auto aip = ip; // aip points to an integer
+const int* cip;
+auto cip = cip // acip is a pointer to a const in (t the value canot be modified but th ememory address i\t points can (?))
+//
+const int* const cicp = ip;
+auto acicp = cicp; // 
