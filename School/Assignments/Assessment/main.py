92d35f062abef0796a7ea9e61ce6397f1e142d8a
NotTacoz <tacozpvp@gmail.com>
2025-04-18 13:34:50 +0800

    made improvements to perlin generation

--- School/Assignments/Assessment/main.py
+++ School/Assignments/Assessment/main.py
@@ -18,29 +18,43 @@ def get_sigmoid(x):
 
 
 def draw_background(size):
+    test = []
+    x_offset = random.uniform(0,10000)
+    y_offset = random.uniform(0,10000)
     # print("drawing background")
     maparr = np.zeros((size,size), dtype=float)
+    mult = 6
     for i in range(len(maparr)):
         for j in range(len(maparr[i])):
-            maparr[i][j] = pnoise2(i/size, j/size, 5)
+            pno = pnoise2((i+x_offset)*mult/size, (j+y_offset)*mult/size, 50)
+            pno = (pno+1)/2
+            maparr[i][j] = pno
+            test.append(maparr[i][j])
 
-            c = round(255 * ((maparr[i][j])+1)/2)
-            if c < 150:
-                c = 0
+            c = round(255 * (pno**1.1))
+            fav = round(255 * (pno**0.5))
 
-            print(c)
+            if c >= 130:
+                col = (c-50, c-50, fav)
+            elif 120 < c <= 129:
+                col = (fav, fav, c)
+            else:
+                col = (c,fav,c)
+
+
+            # print(col)
             
-            col = (c,c,c)
             square.fill(col)
             pixel_draw = pygame.Rect(5*(i+1), 5*(j+1), 15, 15)
             screen.blit(square, pixel_draw)
             # pygame.draw.circle(screen, col, (i*2, j*2), 2)
+    print(np.mean(test))
     # pygame.draw.circle(screen, "black", (30, 30), 500)
     # print(maparr)
 
 def main():
     screen.fill("white")
-    draw_background(64)
+    draw_background(100)
     
     running = True
     while running:
